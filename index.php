5bf647038dd41604186403872044601cde563950
kiddos <kiddo831007@gmail.com>
2019-01-13 02:50:01 +0800

    clean up styling

--- index.php
+++ index.php
@@ -10,18 +10,20 @@
 <body>
   <div class="content">
     <h1 class="header">Vocabulary Tests</h1>
+
     <form action="test.php" method="POST" class="form">
-      <div class="option choice">
+      <div class="option">
         <span>Choose Test Type:</span>
         <select id="test-type" name="test-type">
           <option value="definition">Find Definition</option>
           <option value="sentence">Fit Sentence</option>
         </select>
       </div>
-      <div class="option choice">
+
+      <div class="option">
         <span>Choose Collection: </span>
         <select name="collection" id="collection">
-          <?php 
+          <?php
           require 'include/loader.php';
 
           $files = getAllFiles();
@@ -33,12 +35,15 @@
           ?>
         </select>
       </div>
-      <div class="option questions">
+
+      <div class="option">
         <span>Number of Questions: </span>
         <input id="num-questions" name="num-questions" type="number" value="10">
       </div>
-      <input type="submit" value="Start">
-      <a href="add_word.php" class="add-word">Add Words</a>
+      <input type="submit" value="Start" class="button">
+
+      <a href="review.php" class="button">Review</a>
+      <a href="add_word.php" class="button">Add New Words</a>
     </form>
   </div>
 </body>
